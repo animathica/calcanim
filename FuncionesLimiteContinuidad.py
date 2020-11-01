a5a2e40fa03bfc48cc80927ffd0a90f5685dfb66
Donaldo Mora <odlanod60@gmail.com>
2020-11-01 17:11:34 -0600

    Update FuncionesLimiteContinuidad.py
    Se agregó la clase de uno de los teoremas fuertes, continuas en compactos son acotadas.

--- FuncionesLimiteContinuidad.py
+++ FuncionesLimiteContinuidad.py
@@ -3196,3 +3196,68 @@ class FunContinuasEnAbiertos (Scene):
         self.play(ReplacementTransform(textf1,textf2))
         self.wait(5)
         self.play(FadeOut(textf2))
+
+## Teorema fuerte: Continuas son acotadas en compactos ##
+
+class Continua_y_acotada(Scene):
+    def construct(self):
+        # Textos
+        title = TextMobject('''Continua en compactos implica acotada''')
+        t1 = TextMobject('''$f:A\\subset\\mathbb{R}^n\\to\\mathbb{R}^m$ es acotada si \n
+                            $f[A]\\subset\\mathbb{R}^m$ es acotada''')
+        t2 = TextMobject('''Teniendo en cuenta la definición anterior consideremos el \n 
+                            conjunto $A=[0,\\ 1.5]\\times[0,2]$ en $\\mathbb{R}^2$''')
+        t3 = TextMobject('''¿Puedes probar que este conjunto es compacto?''')
+        t4 = TextMobject('''Toma la función $f:A\\to\\mathbb{R}^2$ dada por $f(x,y)=(x^2,y^2)$, \n 
+                            ¿cuál es la imagen de $A$ bajo $f$?''')
+        t5 = TextMobject('''$f$ es continua y $f[A]$ es un conjunto acotado. \n
+                            Intenta demostrar ambas afirmaciones''')
+        t6 = TextMobject('''El resultado que vimos, es uno de los teoremas importantes \n
+                            de continuidad, y dice lo siguiente:''').shift(UP)
+        t7 = TextMobject('''Si $F:K\\subset\\mathbb{R}^n\\to\\mathbb{R}^m$ es continua y $K$ compacto \n
+                            entonces $F$ es acotada''').next_to(t6,DOWN)
+        
+        # Ejes
+        ejes1 = Axes(x_min=-0.5,x_max=5,y_min=-0.5,y_max=4).move_to((-6+2.25,-3+1.75,0))
+        ejes2 = Axes(x_min=-0.5,x_max=5,y_min=-0.5,y_max=4).move_to((1.5+2.25,-3+1.75,0))
+        
+        # Objetos
+        compacto = Rectangle(height=1.5,width=2,color=RED,fill_color=RED,fill_opacity=0.8).move_to((-5,-2.25,0))
+        imagen = Rectangle(height=2.25,width=4,color=BLUE,fill_color=BLUE,fill_opacity=0.8).move_to((3.5,-1.875,0))
+        flecha = Arrow(start=(-1,0,0),end=(1,0,0),color=WHITE)
+        f = TexMobject(r"f(x,y)").next_to(flecha,DOWN).shift(0.25*DOWN)
+
+        # Grupos
+        Grupo1 = VGroup(t3,t4,t5).to_edge(UP).scale(0.7)
+        Grupo2 = VGroup(flecha,f)
+        Grupo3 = VGroup(ejes1,ejes2,compacto,imagen,Grupo2,t5)
+
+        # Animación
+
+        self.play(Write(title))
+        self.wait()
+        self.play(ReplacementTransform(title,t1))
+        self.wait(2)
+        self.play(ReplacementTransform(t1,t2))
+        self.wait(2)
+        self.play(ApplyMethod(t2.scale,0.7))
+        self.play(ApplyMethod(t2.to_edge,UP))
+        self.wait()
+        self.play(ShowCreation(ejes1))
+        self.play(ShowCreation(compacto))
+        self.wait()
+        self.play(ReplacementTransform(t2,t3))
+        self.wait(2)
+        self.play(ReplacementTransform(t3,t4))
+        self.wait(2)
+        self.play(Write(Grupo2))
+        self.play(ShowCreation(ejes2))
+        self.play(ShowCreation(imagen))
+        self.wait()
+        self.play(ReplacementTransform(t4,t5))
+        self.wait(2)
+        self.play(FadeOut(Grupo3))
+        self.play(Write(t6))
+        self.play(Write(t7))
+        self.wait(3)
+        self.play(FadeOut(t6),FadeOut(t7))
