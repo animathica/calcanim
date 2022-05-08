82f8bec36a1576c87ac3a78e1f281715db769a37
Donaldo Emilio Mora Chávez <donaldo.mora@ciencias.unam.mx>
2022-05-07 19:25:20 -0500

    Actualización mínimos y máximos
    Se actualiza el código para el video mencionado, con las correcciones observadas.

--- Teoremas de diferenciabilidad/max_min.py
+++ Teoremas de diferenciabilidad/max_min.py
@@ -28,10 +28,10 @@ class maximos_minimos (ThreeDScene, Scene):
             self.vector_field = VectorField(
                 campo,
                 #Ajuste de propiedades del campo:
-                x_min=-5,
-                x_max=5,
-                y_min=-3,
-                y_max= 3,
+                x_min=-3,
+                x_max=3,
+                y_min=-2,
+                y_max= 2,
                 delta_x=.25,
                 delta_y=.25,
                 # length_func=linear,
@@ -57,6 +57,7 @@ class maximos_minimos (ThreeDScene, Scene):
             ]),v_min=-1.4,v_max=1.4,u_min=-1.4,u_max=1.4,fill_color=PURPLE_E,fill_opacity=0.75,
             resolution=(30, 30))
         return superficie
+    
     def PrimeraEscena(self):
         titulo = TextMobject("Máximos y Mínimos").scale(1.5)
         texto_1 = TextMobject("Consideremos la siguiente función").move_to(UP)
@@ -95,6 +96,7 @@ class maximos_minimos (ThreeDScene, Scene):
             *[FadeOut(mob)for mob in self.mobjects]
         )
         self.move_camera(phi=0,theta=-90*DEGREES,frame_center=(0,0,0))
+    
     def SegundaEscena(self):
         texto_1 = TextMobject("Consideremos el campo gradiente de la función $f$").move_to(UP)
         texto_2_1 = TexMobject(r"\nabla f:\mathbb{R}^2\to\mathbb{R}^2").next_to(texto_1, DOWN)
@@ -158,22 +160,24 @@ class maximos_minimos (ThreeDScene, Scene):
         self.play(
             *[FadeOut(mob)for mob in self.mobjects]
         )
+    
     def TercerEscena(self):
         texto_1 = TextMobject(''' Ahora aproximamos el campo gradiente de $f$ alrededor \n
                                  de $(0,0)$, utilizando su derivada $D(\\nabla f)$ ''')
-        texto_2 = TextMobject(''' La matriz asociada a $D(\\nabla f)$ se llama matriz hessiana ''')
+        texto_2 = TextMobject(''' La matriz asociada a $D(\\nabla f)$ se llama matriz hessiana.\n
+                                  Definimos entonces $H_f(x,y)=D(\\nabla f)$''')
         texto_3 = TextMobject("En el ejemplo,").move_to(1.5*UP)
-        texto_4 = TexMobject(r""" D(\nabla f)= \begin{pmatrix} 6x & 3 \\ 3 & -6y \end{pmatrix} """)
+        texto_4 = TexMobject(r""" H_f(x,y)= \begin{pmatrix} 6x & 3 \\ 3 & -6y \end{pmatrix} """)
         texto_5 = TextMobject("En particular sobre el punto $(0,0)$,").move_to(1.5*UP)
-        texto_6 = TexMobject(r""" D_{\Vec{0}}(\nabla f)= \begin{pmatrix} 0 & 3 \\ 3 & 0 \end{pmatrix}, """)
-        texto_6_1 = TextMobject("donde $\\text{det}(D_{\\Vec{0}}(\\nabla f))< 0$").move_to(1.5*DOWN)
+        texto_6 = TexMobject(r""" H_f(0,0)= \begin{pmatrix} 0 & 3 \\ 3 & 0 \end{pmatrix}, """)
+        texto_6_1 = TextMobject("donde $\\text{det}(H_f(0,0))< 0$").move_to(1.5*DOWN)
         texto_7 = TextMobject("Veamos cómo la hessiana aproxima al campo gradiente de $f$")
-        texto_7_1 = TexMobject(r"D_{\Vec{0}}(\nabla f)(x,y)=(3y,3x)").move_to(3*DOWN)
+        texto_7_1 = TexMobject(r"H_f(0,0)\begin{pmatrix} x \\ y \end{pmatrix}=\begin{pmatrix} 3y \\ 3x \end{pmatrix}").move_to(3*DOWN)
         texto_7_1.bg = SurroundingRectangle(
             texto_7_1, color=WHITE, fill_color=BLACK, fill_opacity=1
         )
         caja_texto_7_1 = VGroup(texto_7_1.bg, texto_7_1)
-        texto_8 = TextMobject("Como $f\\in C^2$, $\\nabla f\\in C^1$ y $\\text{det}(D_{\\Vec{0}}(\\nabla f))\\neq 0$").move_to(3*UP)
+        texto_8 = TextMobject("Como $f\\in C^2$, $\\nabla f\\in C^1$ y $\\text{det}(H_f(0,0))\\neq 0$").move_to(3*UP)
         texto_8.bg = SurroundingRectangle(
             texto_8, color=WHITE, fill_color=BLACK, fill_opacity=1
         )
@@ -183,15 +187,57 @@ class maximos_minimos (ThreeDScene, Scene):
         texto_9.bg = SurroundingRectangle(
             texto_9, color=WHITE, fill_color=BLACK, fill_opacity=1
         )
-        caja_texto_9 = VGroup(texto_9.bg, texto_9)                       
+        caja_texto_9 = VGroup(texto_9.bg, texto_9) 
+        texto_10 = TextMobject('''En el ejemplo, el campo gradiente y el campo de la Hessiana \n
+                                  en el origen, tienen un punto silla en el origen, es decir, \n
+                                  cerca del origen hay flechas que apuntan hacia él pero \n
+                                  también flechas que se alejan de él''').scale(0.8).to_edge(UP)
+        texto_10.bg = SurroundingRectangle(
+            texto_10, color=WHITE, fill_color=BLACK, fill_opacity=1
+        )
+        caja_texto_10 = VGroup(texto_10.bg,texto_10)
         #Objetos
-        axes = NumberPlane()
-        punto_critico = Dot(color=RED)
-        #Campo Gradiente de f aproximado por hessiana
+        # Ejes gradiente
+        axes = NumberPlane(
+            x_min = -3.5,
+            x_max = 3.5,
+            y_min = -2.5,
+            y_max = 2.5
+        )\
+            .scale(0.8)\
+            .to_edge(LEFT)
+        # Ejes aprox Hessiana
+        axes2 = NumberPlane(
+            x_min = -3.5,
+            x_max = 3.5,
+            y_min = -2.5,
+            y_max = 2.5
+        )\
+            .scale(0.8)\
+            .to_edge(RIGHT)
+        # Campo gradiente
+        def campo_grad(point):
+            x, y = point[:2]
+            return 3*y+3*x**2 * RIGHT + 3*x-3*y**2 * UP
+        campograd = self.get_vector_field(campo_grad)\
+            .scale(0.85)\
+            .to_edge(LEFT)
+        label1 = TextMobject('''Campo \n 
+                                gradiente''',color=BLUE).scale(0.6)
+        label1.bg = SurroundingRectangle(label1,color=WHITE,fill_color=BLACK,fill_opacity=1)
+        label1_caja = VGroup(label1.bg,label1).move_to((-4,-2.5,0))
+        # Campo Gradiente de f aproximado por hessiana
         def campo(point):
             x, y = point[:2]
             return 3*y * RIGHT + 3*x * UP
-        campo = self.get_vector_field(campo)
+        campo = self.get_vector_field(campo)\
+            .scale(0.85)\
+            .to_edge(RIGHT)
+        label2 = TextMobject('''Campo de la \n 
+                                Hessiana''',color=BLUE).scale(0.6)
+        label2.bg = SurroundingRectangle(label2,color=WHITE,fill_color=BLACK,fill_opacity=1)
+        label2_caja = VGroup(label2.bg,label2).move_to((4,-2.5,0))
+        
         #Animacion
         self.play(Write(texto_1))
         self.wait(7)
@@ -213,8 +259,8 @@ class maximos_minimos (ThreeDScene, Scene):
         self.play(Write(texto_7))
         self.wait(5)
         self.play(FadeOut(texto_7))
-        self.play(ShowCreation(axes))
-        self.play(ShowCreation(campo))
+        self.play(ShowCreation(axes),ShowCreation(axes2))
+        self.play(ShowCreation(campo),ShowCreation(campograd),Write(label1_caja),Write(label2_caja))
         self.play(ShowCreation(caja_texto_7_1))
         self.wait(3)
         self.play(ShowCreation(caja_texto_8))
@@ -222,11 +268,15 @@ class maximos_minimos (ThreeDScene, Scene):
         self.play(FadeOut(caja_texto_8))
         self.play(ShowCreation(caja_texto_9))
         self.wait(9)
+        self.play(FadeOut(caja_texto_9))
+        self.play(ShowCreation(caja_texto_10))
+        self.wait(13)
         self.play(
             *[FadeOut(mob)for mob in self.mobjects]
         )
+    
     def CuartaEscena (self):
-        texto_1 = TextMobject(''' En este caso $\\text{det}(D_{\\Vec{0}}(\\nabla f))< 0$, por lo tanto,\n
+        texto_1 = TextMobject(''' En este caso $\\text{det}(H_f(0,0))< 0$, por lo tanto,\n
                                      el punto $(0,0)$ es un punto silla de $f$ ''').move_to(3*UP)
         texto_1.bg = SurroundingRectangle(
             texto_1, color=WHITE, fill_color=BLACK, fill_opacity=1
@@ -236,8 +286,8 @@ class maximos_minimos (ThreeDScene, Scene):
                                  clase $C^2$,  con un punto crítico $\\Vec{x_0}$ en el interior de $A$ y \n
                                 $H(\\Vec{x_0})$ la matriz hessiana en $\\Vec{x_0}$,''').move_to(2*UP)
         texto_3 = TextMobject(''' i) Si $\\text{det}(H(\\Vec{x_0}))< 0$, $\\Vec{x_0}$ es un punto silla''').next_to(texto_2,1.5*DOWN)
-        texto_4 = TextMobject(''' ii) Si $\\text{det}(H(\\Vec{x_0}))> 0$ y $(H(\\Vec{x_0}))_{11}$>0, $\\Vec{x_0}$ es un mínimo local''').next_to(texto_3,1*DOWN)                       
-        texto_5 = TextMobject(''' iii) Si $\\text{det}(H(\\Vec{x_0}))> 0$ y $(H(\\Vec{x_0}))_{11}$<0, $\\Vec{x_0}$ es un máximo local''').next_to(texto_4,1*DOWN)          
+        texto_4 = TextMobject(''' ii) Si $\\text{det}(H(\\Vec{x_0}))> 0$ y $\dfrac{\\partial^2 f}{\\partial x^2}(\\Vec{x_0})>0$, $\\Vec{x_0}$ es un mínimo local''').next_to(texto_3,1*DOWN)                       
+        texto_5 = TextMobject(''' iii) Si $\\text{det}(H(\\Vec{x_0}))> 0$ y $\dfrac{\\partial^2 f}{\\partial x^2}(\\Vec{x_0})<0$, $\\Vec{x_0}$ es un máximo local''').next_to(texto_4,1*DOWN)          
         texto_6 = TextMobject(''' iv) Si $\\text{det}(H(\\Vec{x_0}))= 0$, este criterio no es aplicable''').next_to(texto_5,1*DOWN)          
 
         #Objetos
@@ -270,6 +320,7 @@ class maximos_minimos (ThreeDScene, Scene):
         self.play(
             *[FadeOut(mob)for mob in self.mobjects]
         )
+    
     def QuintaEscena (self):
         texto_1 = TextMobject(''' Ahora consideremos una superficie dada por \n
                                 la gráfica de la siguiente función ''').move_to(1.5*UP)
@@ -279,11 +330,11 @@ class maximos_minimos (ThreeDScene, Scene):
         texto_3 = TextMobject(''' Podemos obtener el campo gradiente, así como\n
                              la matriz hessiana de la función $f$''').move_to(1.5*UP)
         texto_4 = TexMobject(r"\nabla f(x,y)=(4x^3,4y^3)")
-        texto_5 = TexMobject(r""" D(\nabla f)= \begin{pmatrix} 12x^2 & 0 \\ 0 & 12y^2 \end{pmatrix} """).next_to(texto_4,DOWN)
+        texto_5 = TexMobject(r""" H_f(x,y)= \begin{pmatrix} 12x^2 & 0 \\ 0 & 12y^2 \end{pmatrix} """).next_to(texto_4,DOWN)
         texto_6 = TextMobject(''' Obsérvese que en el punto $(0,0)$ \n 
                                 hay un punto crítico de $f$ ''').move_to(1.5*UP)
         texto_7 = TextMobject(''' Pero en $(0,0)$ la hessiana se vuelve ''').move_to(1.5*UP)
-        texto_8 = TexMobject(r""" D_{\Vec{0}}(\nabla f)= \begin{pmatrix} 0 & 0 \\ 0 & 0 \end{pmatrix} """).next_to(texto_4,DOWN)
+        texto_8 = TexMobject(r""" H_f(0,0)= \begin{pmatrix} 0 & 0 \\ 0 & 0 \end{pmatrix} """).next_to(texto_4,DOWN)
         texto_9 = TextMobject(''' En este caso la matriz hessiana no nos permite clasificar el \n
                                 punto crítico, por lo tanto el criterio no es siempre aplicable ''')
         #Objetos
@@ -330,6 +381,7 @@ class maximos_minimos (ThreeDScene, Scene):
         self.play(Write(texto_9))
         self.wait(9)
         self.play(FadeOut(texto_9))
+    
     def construct(self):
         self.PrimeraEscena()
         self.SegundaEscena()
